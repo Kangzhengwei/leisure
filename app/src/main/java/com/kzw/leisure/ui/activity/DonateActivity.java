e99cb2980be82af3e68495be96d9db76088dec3a
kangzhengwei <kang476793909@hotmail.com>
2019-12-16 13:44:17 +0800

    完成视频模块，书籍搜索

--- app/src/main/java/com/kzw/leisure/ui/activity/DonateActivity.java
+++ app/src/main/java/com/kzw/leisure/ui/activity/DonateActivity.java
@@ -1,12 +1,15 @@
 package com.kzw.leisure.ui.activity;
 
 import android.os.Bundle;
+
 import com.kzw.leisure.R;
 import com.kzw.leisure.base.BaseActivity;
 
 public class DonateActivity extends BaseActivity {
 
 //FKX00764DNFNQTZFDFJSAA
+
+    String alipaycode = "https://raw.githubusercontent.com/Kangzhengwei/leisure/master/app/src/main/assets/alipay_code.jpg";
 
     @Override
     protected int getContentView() {
