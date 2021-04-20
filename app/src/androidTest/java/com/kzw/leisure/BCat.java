d876b65b37a5f95f5fef1dd3087f50af71f279ac
kang4 <kang476793909@hotmail.com>
2021-04-20 23:37:57 +0800

    conmit

--- app/src/androidTest/java/com/kzw/leisure/BCat.java
+++
@@ -1,8 +0,0 @@
-package com.kzw.leisure;
-
-public class BCat extends Cat {
-    @Override
-    public void move(int x, int y) {
-        super.move(x, y);
-    }
-}
--- app/src/androidTest/java/com/kzw/leisure/CCat.java
+++
@@ -1,9 +0,0 @@
-package com.kzw.leisure;
-
-public class CCat extends Cat {
-
-    @Override
-    public void move(int x, int y) {
-        super.move(x, y);
-    }
-}
